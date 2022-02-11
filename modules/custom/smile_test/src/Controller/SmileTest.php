2472c56ff57b86c023108e6fe84e3134a0036d03
melnichukbohdan <melnichukbogdan1@gmail.com>
2022-02-11 19:01:49 +0200

    3. Add new custom route “/smile-test/node-render/[nid]” which loads and show the node.

--- modules/custom/smile_test/src/Controller/SmileTest.php
+++ modules/custom/smile_test/src/Controller/SmileTest.php
@@ -12,14 +12,35 @@ namespace Drupal\smile_test\Controller;
  * Provide route for my custom routs
  */
 
-use Drupal\Core\Controller\ControllerBase;
+class SmileTest {
 
-class SmileTest extends ControllerBase {
-
-  public function content () {
+  /*
+   * Display simple text message 'It is my first route ever'
+   */
+  public function content() {
     return array(
       '#markup' => '<h2>It is my first route ever</h2>'
     );
   }
 
+  /*
+   * @param nid - node number
+   * Displays node teaser which number entered user
+   * if node number doesn'n exist - displays message 'Node does not exist :('
+   */
+  public function nodeRender($nid) {
+
+      $node = \Drupal::entityTypeManager()
+        ->getStorage('node')
+        ->load($nid);
+      if ($node) {
+        $element = \Drupal::entityTypeManager()
+          ->getViewBuilder('node')
+          ->view($node, 'teaser');
+        $output = \Drupal::service('renderer')->render($element);
+        return ['#markup' => $output];
+      }
+      return ['#markup' => '<h2>Node does not exist :(</h2>'];
+
+  }
 }
