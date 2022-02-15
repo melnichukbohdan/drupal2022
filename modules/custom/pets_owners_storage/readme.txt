5914b45f369db58b283998c21537192fb0eb5fe4
melnichukbohdan <melnichukbogdan1@gmail.com>
2022-02-15 13:52:27 +0200

    3. Create administration page that will show all the records from your table

--- modules/custom/pets_owners_storage/readme.txt
+++ modules/custom/pets_owners_storage/readme.txt
@@ -1,3 +1,13 @@
 useful link:
 
 https://codimth.com/blog/web/drupal/how-use-database-api-creating-custom-form-crud-operations-drupal-8
+
+3. //get data
+         $rows[] = [
+           'id' => $data->poid,
+           ........
+
+           https://qna.habr.com/q/187405
+
+
+
--- modules/custom/pets_owners_storage/src/Controller/PetsOwnersStorage.php
+++ modules/custom/pets_owners_storage/src/Controller/PetsOwnersStorage.php
@@ -2,13 +2,53 @@
 
 namespace Drupal\pets_owners_storage\Controller;
 
-class PetsOwnersStorage
-{
+class PetsOwnersStorage {
 
-  public function displayTable(){
-    return array(
-      '#markup' => '<h2>pets_owners_storage</h2>'
-    );
+  public function displayTable() {
+
+    $form =[];
+    //create table header
+    $header_table = [
+      'id' => t('ID'),
+      'name' => t('Name'),
+      'gender' => t('Gender'),
+      'age' => t('Age'),
+      'father' => t('Father'),
+      'mother' => t('Mother'),
+      'pet_name' => t('Pet Name'),
+      'email' => t('Email'),
+    ];
+
+    // get data from database
+    $connection = \Drupal::database()->select('pets_owners_storage', 'p');
+    $query = $connection->fields('p',
+      ['poid', 'name', 'gender', 'age', 'father', 'mother', 'pet_name', 'email']);
+    $results = $query->execute()->fetchAll();
+    $rows = [];
+    foreach ($results as $data) {
+
+      //get data
+      $rows[] = [
+        'id' => $data->poid,
+        'name' => $data->name,
+        'gender' => $data->gender,
+        'age' => $data->age,
+        'father' => $data->father,
+        'mother' => $data->mother,
+        'pet_name' => $data->pet_name,
+        'email' => $data->email,
+      ];
+    }
+      // render table
+      $form['table'] = [
+        '#type' => 'table',
+        '#header' => $header_table,
+        '#rows' => $rows,
+
+      ];
+      return $form;
+
+
   }
 
 }
