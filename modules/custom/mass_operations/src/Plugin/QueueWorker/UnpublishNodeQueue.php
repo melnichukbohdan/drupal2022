3089a7217b35e1f0033ad3295464d823dce32bb0
melnichukbohdan <melnichukbogdan1@gmail.com>
2022-04-25 10:58:54 +0300

    Queue API + Batch API
    Task 3.
    Create UnpublishNodeQueue queue and worker that will unpublish nodes

---
+++ modules/custom/mass_operations/src/Plugin/QueueWorker/UnpublishNodeQueue.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\mass_operations\Plugin\QueueWorker;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\NodeInterface;
+
+/**
+ *
+ * @QueueWorker(
+ *   id = "node_unpublisher",
+ *   title = @Translation("Node UnPublisher"),
+ *   cron = {"time" = 30}
+ * )
+ */
+
+ class UnpublishNodeQueue extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+   /**
+    * The node storage.
+    *
+    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+    */
+   protected $nodeStorage;
+
+    /**
+    * Creates a new Node Unpublish.
+    *
+    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $nodeStorage
+    *   The node storage.
+    */
+   public function __construct(EntityTypeManagerInterface $nodeStorage) {
+       $this->nodeStorage = $nodeStorage;
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+     return new static(
+       $container->get('entity_type.manager')
+     );
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public function processItem($data) {
+     /** @var EntityPublishedInterface $nodes */
+     $nodes = $this->nodeStorage->getStorage('node')->load($data);
+     if ($nodes instanceof NodeInterface && $nodes->isPublished()) {
+        $nodes->load($data)
+         ->setUnpublished()
+         ->save();
+
+     }
+   }
+ }
+
