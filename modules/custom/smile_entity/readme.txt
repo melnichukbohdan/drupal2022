8030b6654592cb946180cf01c96de795c2b4dbd2
melnichukbohdan <melnichukbogdan1@gmail.com>
2022-06-13 17:16:22 +0300

    Practice 1: Create a custom module with the following requirements
    task 6. Show block with N latest entities, make N (number of entities) configurable, show this block in the left sidebar on module install (use directory config/install).

--- modules/custom/smile_entity/readme.txt
+++ modules/custom/smile_entity/readme.txt
@@ -5,3 +5,10 @@ task 5
 https://www.droptica.com/blog/users-roles-and-permissions-drupal-most-important-informations-drupal-developer/
 
 
+task 6
+-DependencyInjection for block
+https://www.drupal.org/docs/drupal-apis/services-and-dependency-injection/dependency-injection-in-plugin-block
+-render entity for block
+https://drupal.stackexchange.com/questions/291547/render-blocks-of-custom-type
+-set default configuration for block
+https://www.drupal.org/docs/creating-custom-modules/step-by-step-tutorial-hello-world/add-a-default-configuration
---
+++ modules/custom/smile_entity/src/Plugin/Block/SmileEntityBlock.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\smile_entity\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a smile entity block.
+ *
+ * @Block(
+ *   id = "smile_entity_block",
+ *   admin_label = @Translation("Smile Entity"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class SmileEntityBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var AccountInterface $currentUser
+   */
+  protected $currentUser;
+
+  /**
+   * @var EntityTypeManagerInterface $entityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param AccountInterface $currentUser
+
+   * @param EntityTypeManagerInterface $entityTypeManager
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition,
+                              AccountInterface $currentUser,
+                              EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $currentUser;
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['items'] = [
+      '#type' => 'number',
+      '#title' => $this->t("How many 'Smile Entity' do you want load?"),
+      '#default_value' => $this->configuration['items'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['items'] = $form_state->getValue('items');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $entityTypeID = 'smile_entity';
+    $query = $this->getEntityTypeManager()->getStorage($entityTypeID)->getQuery();
+    $results = $query->condition('role', $this->getCurrentUser()->getRoles(), 'IN')
+      ->range(0, $this->configuration['items'])
+      ->sort('id', 'DESC')
+      ->execute();
+
+    $entitys = $this->getEntityTypeManager()->getStorage($entityTypeID)->loadMultiple($results);
+    $build[] = $this->getEntityTypeManager()->getViewBuilder($entityTypeID)->viewMultiple($entitys);
+
+    return[
+      'elements' => $build,
+    ];
+  }
+
+  /**
+   * @return AccountInterface
+   */
+  public function getCurrentUser(): AccountInterface  {
+    return $this->currentUser;
+  }
+
+  /**
+   * @return EntityTypeManagerInterface
+   */
+  public function getEntityTypeManager(): EntityTypeManagerInterface  {
+    return $this->entityTypeManager;
+  }
+
+}
